6b5c2e42ce936675bfbaa9de808b9f496c1216df
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2024-04-23 01:07:25 +0900

    テーブル3つ完成済

--- apis/bases/exercise.py
+++ apis/bases/exercise.py
@@ -6,5 +6,5 @@ from apis.bases.base import Base
 class Exercise(Base):
     __tablename__ = "exercise"  # テーブル名 __tablename__はsqlalchemyの特別な変数
     __table_args__ = {"extend_existing": True}  # 既存テーブルの再定義を認める。
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(unsigned=True), primary_key=True)
     exercise_name = Column(String(64), nullable=False, index=True)
