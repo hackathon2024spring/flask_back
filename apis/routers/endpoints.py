90ae1b2333c4e061f0c9e4f1f796b78ba4f3127a
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2024-05-03 00:07:57 +0900

    message,channel関連のテーブルとAPIを削除

--- apis/routers/endpoints.py
+++ apis/routers/endpoints.py
@@ -32,46 +32,7 @@ class Login:
     summary = "ログイン"
     description = """ログイン"""
 
-
 class Signout:
     endpoint = "/signout"
     summary = "サインアウト"
     description = """サインアウト"""
-
-
-# 以下はTODOアプリ用コード
-
-class AddChannel:
-    endpoint = "/channel"
-    summary = "掲示板の追加"
-    description = """掲示板の追加"""
-
-
-class AddMessage:
-    endpoint = "/message"
-    summary = "メッセージの追加"
-    description = """メッセージの追加"""
-
-
-class DelMessage:
-    endpoint = "/message"
-    summary = "メッセージの削除"
-    description = """メッセージの削除"""
-
-
-class DelChannel:
-    endpoint = "/channel"
-    summary = "掲示板の削除"
-    description = """掲示板の削除"""
-
-
-class GetChannels:
-    endpoint = "/channels"
-    summary = "掲示板の取得"
-    description = """掲示板の取得"""
-
-
-class GetMessages:
-    endpoint = "/messages"
-    summary = "メッセージの取得"
-    description = """メッセージの取得"""
