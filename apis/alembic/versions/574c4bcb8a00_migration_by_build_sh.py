3d50a9cd7358f5e799e4ddfe83a602e4f358a449
Tanaka Naoki <tanakanaoki@heavisidelab.jp>
2024-05-24 13:23:23 +0900

    update testcode

--- apis/alembic/versions/574c4bcb8a00_migration_by_build_sh.py
+++ apis/alembic/versions/574c4bcb8a00_migration_by_build_sh.py
@@ -1,8 +1,8 @@
 """migration by build.sh
 
-Revision ID: 98a65ecad90b
+Revision ID: 574c4bcb8a00
 Revises: 
-Create Date: 2024-05-23 21:51:51.851668
+Create Date: 2024-05-24 09:06:26.713942
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision: str = '98a65ecad90b'
+revision: str = '574c4bcb8a00'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- apis/tests/test_fastapi.py
+++ apis/tests/test_fastapi.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi import FastAPI, status
-from httpx import AsyncClient, ASGITransport
+from httpx import AsyncClient, ASGITransport, Cookies
 from ddd.router.hello import view as view0
 from ddd.router.signup import view as view1
 from ddd.router.login import view as view2
@@ -36,6 +36,94 @@ async def test_hello():
 
 
 @pytest.mark.asyncio
+async def test_signup_illegal_email():
+    """
+    Test:   ユーザーの新規登録でemailが無い
+    Expect: 不適切なemailで失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/signup",
+            json={
+                "username": "testuser",
+                "email": "",
+                "password1": "testpass",
+                "password2": "testpass",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "value is not a valid email address" in data["detail"]
+
+
+@pytest.mark.asyncio
+async def test_signup_illegal_username():
+    """
+    Test:   ユーザーの新規登録でusernameが無い
+    Expect: ユーザー登録が失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/signup",
+            json={
+                "username": "",
+                "email": "test@example.com",
+                "password1": "testpass",
+                "password2": "testpass",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "ユーザー名は半角英数と_.+のみです" in data["detail"]
+
+
+@pytest.mark.asyncio
+async def test_signup_short_password():
+    """
+    Test:   ユーザーの新規登録でパスワードが短すぎる
+    Expect: ユーザー登録が失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/signup",
+            json={
+                "username": "testuser",
+                "email": "test@example.com",
+                "password1": "test",
+                "password2": "test",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "パスワードは8文字以上です" in data["detail"]
+
+
+@pytest.mark.asyncio
+async def test_signup_wrong_password():
+    """
+    Test:   ユーザーの新規登録でconfrim_passwordが不一致
+    Expect: ユーザー登録が失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/signup",
+            json={
+                "username": "testuser",
+                "email": "test@example.com",
+                "password1": "testpass",
+                "password2": "hogehoge",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "パスワードが一致しません" in data["detail"]
+
+
+@pytest.mark.asyncio
 async def test_signup_success():
     """
     Test:   ユーザーの新規登録
@@ -55,6 +143,68 @@ async def test_signup_success():
         data = response.json()
         assert response.status_code == status.HTTP_200_OK
         assert data["data"]["username"] == "testuser"
+
+
+@pytest.mark.asyncio
+async def test_signup_again():
+    """
+    Test:   同じユーザーの登録
+    Expect: ユーザー登録が失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/signup",
+            json={
+                "username": "testuser",
+                "email": "test@example.com",
+                "password1": "testpass",
+                "password2": "testpass",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "Emailアドレス・パスワードに問題があります。" in str(data["detail"])
+
+
+@pytest.mark.asyncio
+async def test_login_wrong_password():
+    """
+    Test:   使えないパスワードでログイン
+    Expect: ログインが失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/login",
+            json={
+                "email": "test@example.com",
+                "password": "hogehoge",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "Emailアドレス・パスワードに問題があります" in str(data["detail"])
+
+
+@pytest.mark.asyncio
+async def test_login_wrong_email():
+    """
+    Test:   使えないemailアドレスでログイン
+    Expect: ログインが失敗したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        response = await client.post(
+            "/login",
+            json={
+                "email": "hoge@example.com",
+                "password": "testpass",
+            },
+        )
+        data = response.json()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "Emailアドレス・パスワードに問題があります" in str(data["detail"])
 
 
 @pytest.mark.asyncio
@@ -241,12 +391,36 @@ async def test_get_calendar():
         # 前のテストで書き換えた内容が反映されている。
         response = await client.get("/calendars/2024/5")
         data = response.json()
-        print(data)
-        # assert response.status_code == status.HTTP_200_OK
-        # assert data["status"] == 1
-        # assert data["data"][0] == {
-        #     "exerciseId": 1,
-        #     "exerciseName": "階段を使う",
-        #     "exerciseDone": True,
-        # }
-        assert True
+        assert response.status_code == status.HTTP_200_OK
+        assert data["status"] == 1
+        assert data["data"][23] == {"day": "2024-05-24", "exerciseDone": True}
+
+
+@pytest.mark.asyncio
+async def test_logout_success():
+    """
+    Test:   ログアウト
+    Expect: ログアウトが成功したレスポンスが帰ること。
+    """
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as client:
+        # まずログインしてクッキーを取得
+        login_response = await client.post(
+            "/login",
+            json={
+                "email": "test@example.com",
+                "password": "testpass",
+            },
+        )
+        cookies = Cookies()
+        cookies.update(login_response.cookies)
+
+        # クッキーをクライアントインスタンスに設定
+        client.cookies = cookies
+
+        # /logout にアクセス
+        logout_response = await client.get("/signout")
+        data = logout_response.json()
+        assert logout_response.status_code == status.HTTP_200_OK
+        assert data["message"] == "Successfully logged out"
+        assert logout_response.cookies.get("access_token") is None
